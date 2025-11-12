e6035979677cf79288b0e41931a079ee5689681a
novasup666 <noe.vincent@protonmail.com>
2025-11-12 14:19:18 +0100

    connection to google sheets

--- src/Optimac_API.py
+++ src/Optimac_API.py
@@ -2,6 +2,9 @@ from Optimac import allocate_action_to_participant,update_action_proportions
 import pandas as pan
 #import portalocker
 from participant_real import Participant
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
 
 allocated_actions_file_name = "../data/allocated_actions.csv"
 action_stats_file_name = "../data/action_stats.csv"
@@ -13,36 +16,45 @@ data.csv stores the allocated actions with following columns: n, action_label, m
 
 def add_participant(motivations):
 
-    with open(allocated_actions_file_name, "r+") as f :
-        # portalocker.lock(f, portalocker.LOCK_EX)  # Lock to protect data.csv and ensure the right order of the action assignment
-        with open(action_stats_file_name,"r+") as g: 
-            # portalocker.lock(g, portalocker.LOCK_EX)  # Lock to protect data.csv and ensure the right order of the action assignment
+    conn = st.connection("gsheets", type=GSheetsConnection)
+
+    # with open(allocated_actions_file_name, "r+") as f :
+    #     # portalocker.lock(f, portalocker.LOCK_EX)  # Lock to protect data.csv and ensure the right order of the action assignment
+    #     with open(action_stats_file_name,"r+") as g: 
+    #         # portalocker.lock(g, portalocker.LOCK_EX)  # Lock to protect data.csv and ensure the right order of the action assignment
             
             
             #transform the csv files to compatible format for the algorithm
-            allocationDF = pan.read_csv(allocated_actions_file_name)
-            allocated_actions  = allocationDF.values.tolist()
 
-            actionstatsDF = pan.read_csv(action_stats_file_name)
-            list_actions = {line[0]:{'target':line[1], 'current':line[2],'minimum':line[3]} for line in actionstatsDF.values}
+    #allocationDF = pan.read_csv(allocated_actions_file_name)
+    allocationDF = conn.read(ttl=0,usecols=[0, 1,2],worksheet="allocated_actions")
 
-            n = len(allocationDF)
+    allocated_actions  = allocationDF.values.tolist()
 
-            new_participant = Participant(n,motivations)
-            allocate_action_to_participant(new_participant.to_dict(),n,list_actions, allocated_actions)
-            allocated_action = allocated_actions[-1]
-            allocationDF.loc[n] = allocated_action
+    #actionstatsDF = pan.read_csv(action_stats_file_name)
+    actionstatsDF = conn.read(ttl=0,usecols=[0, 1,2,3],worksheet="actions_stats")
+    list_actions = {line[0]:{'target':line[1], 'current':line[2],'minimum':line[3]} for line in actionstatsDF.values}
 
-            #update_action_proportions(n,list_actions,allocated_action[1])
+    n = len(allocationDF)
 
-            for i in range(len(actionstatsDF)):
-                print(list_actions[ actionstatsDF.loc[i,"action_name"]]["current"])
-                actionstatsDF.loc[i,"current_proportion"] = list_actions[ actionstatsDF.loc[i,"action_name"]]["current"]
+    new_participant = Participant(n,motivations)
+    allocate_action_to_participant(new_participant.to_dict(),n,list_actions, allocated_actions)
+    allocated_action = allocated_actions[-1]
+    allocationDF.loc[n] = allocated_action
 
-            actionstatsDF.to_csv(action_stats_file_name,index=False)
-            allocationDF.to_csv(allocated_actions_file_name, index= False)
+    #update_action_proportions(n,list_actions,allocated_action[1])
 
-            return allocated_action[1]
+    for i in range(len(actionstatsDF)):
+        print(list_actions[ actionstatsDF.loc[i,"action_name"]]["current"])
+        actionstatsDF.loc[i,"current_proportion"] = list_actions[ actionstatsDF.loc[i,"action_name"]]["current"]
+
+    # actionstatsDF.to_csv(action_stats_file_name,index=False)
+    # allocationDF.to_csv(allocated_actions_file_name, index= False)
+
+    actionstatsDF = conn.update(worksheet="actions_stats", data = actionstatsDF)
+    allocationDF = conn.update(worksheet="allocated_actions", data = allocationDF)
+    st.cache_data.clear()
+    return allocated_action[1]
             # portalocker.unlock(g)  # Lock to protect data.csv and ensure the right order of the action assignment
 
         # portalocker.unlock(f)  # Libère le verrou (optionnel, libéré à la fermeture)
