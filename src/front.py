9a20545b87ab03fd56dd3049705b30701d96cef5
novasup666 <noe.vincent@protonmail.com>
2025-11-13 13:54:17 +0100

    Ca avance bien

--- src/front.py
+++ src/front.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from Optimac_API import add_participant, add_feedback
 
-st.title("Campagne fictive d'action participative")
+st.title("Campagne d'opinion: Action participative")
 
 if "count" not in st.session_state:
     st.session_state.count = 0
@@ -35,8 +35,16 @@ if "res" not in st.session_state:
 
 if st.session_state.count == 0:
     st.markdown("""
-    blabla
+    Le projet OPTIMAC est mené par l'équipe DRUID de l'IRISA. Son objectif est de fournir des outils pour la mise en place de campagnes d'action participatives. Ces campagnes ont la particularité de faire participer les citoyens à la production de données scientifiques au travers de la réalisation concrète d'expériences. Par exemple: tondre son gazon à une certaine fréquence (ex 1fois/semaine) et tenir compte de l'évolution de la biodiversité.
+    Au sein du projet optimac nous essayons de modéliser le comportement de participants à de telles campagnes pour pouvoir affiner la méthode avec laquelle attribuer les expériences aux participants.
+    Ainsi, vous allez participer à deux campagnes fictives:
 
+    D'abord: disons que vous ayez un jardin : quelle fréquence de tonte seriez vous d'accord d'adopter le temps d'une expérience d'un mois (avec prise de mesures de biodiversité).
+
+    Ensuite: à quelle fréquence seriez vous prêts à placer de la nourriture pour oiseaux au bords de votre fenetre (et de réaliser des mesures de biodiversité) au cours d'un mois.
+
+    Vous allez devoir juger les différentes propositions, nous vous demandons de le faire le plus honnètement possible, en essayant de vous projeter. N'hésitez pas à prendre votre temps!
+    Ensuite vous vous verrez attribué une action et nous vous demanderons d'indiquer si vous penser que vous mettriez vraiment cette action en place ou si vous pensez que vous n'arriverez pas à vous y tenir. De la même façon, soyez le plus honnête et n'hésitez pas à prendre votre temps.
     """)
 
     motivations = {"weekly":0, "monthly":0,"notatall":0} 
