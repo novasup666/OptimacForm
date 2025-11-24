7cf5376de7184804a2f28da14c2a72cb61d94c2a
novasup666 <noe.vincent@protonmail.com>
2025-11-24 13:46:21 +0100

    Typos

--- src/config.py
+++ src/config.py
@@ -10,33 +10,35 @@ meaningful_actions = [
 
 verbose = [
     """
-## Expérimentation fictive numéro 1: la tonte de gazon
+## Expérimentation fictive numéro 1 : la tonte de gazon
+
 
 D'abord: disons que vous ayez un jardin. 
 
 Votre tâche serait la suivante :
 - Tondre votre gazon à la fréquence demandée.
-- Effectuer un inventaire des insectes et des plantes sur une surface donnée une fois toute les deux semaines.
+- Effectuer un inventaire des insectes et des plantes sur une surface donnée une fois toutes les deux semaines.
 
-Quelle fréquence de tonte seriez vous d'accord d'adopter le temps d'une expérience d'un mois ?
 
-Vous allez devoir juger les différentes propositions, nous vous demandons de le faire le plus honnètement possible, en essayant de vous projeter. N'hésitez pas à prendre votre temps!
+Quelle fréquence de tonte seriez-vous d'accord d'adopter le temps d'une expérience d'un mois ?
 
-Ensuite, nous vous demanderons d'indiquer si vous penser que vous mettriez vraiment cette action en place ou si vous pensez que vous n'arriverez pas à vous y tenir. 
+Vous allez devoir juger les différentes propositions, nous vous demandons de le faire le plus honnêtement possible, en essayant de vous projeter. N'hésitez pas à prendre votre temps!
+
+Ensuite, nous vous demanderons d'indiquer si vous pensez que vous mettriez vraiment cette action en place ou si vous pensez que vous n'arriverez pas à vous y tenir. 
 De la même façon, soyez le plus honnête et n'hésitez pas à prendre votre temps.
-
 """,
 """
-## Expérimentation fictive numéro 2: le nourrissage d'oiseaux
+## Expérimentation fictive numéro 2 : le nourrissage d'oiseaux
+
 
 Votre tâche serait la suivante:
-- Placer une certaine quantité de graînes pour oiseaux sur un rebord de fenêtre, votre balcon ou votre terrasse, selon la fréquence demandée.
-- Effectuer un décompte du nombre d'oiseaux apperçus autour de votre logement, une fois par semaine pendant une vingtaine de minutes.
+- Placer une certaine quantité de graines pour oiseaux sur un rebord de fenêtre, votre balcon ou votre terrasse, selon la fréquence demandée.
+- Effectuer un décompte du nombre d'oiseaux aperçus autour de votre logement, une fois par semaine pendant une vingtaine de minutes.
 
-À quelle fréquence voudriez-vous placer des graînes sur votre rebord de fenêtre ou balcon ?
+À quelle fréquence voudriez-vous placer des graines sur votre rebord de fenêtre ou balcon ?
 
-Vous allez devoir juger les différentes propositions, nous vous demandons de le faire le plus honnètement possible, en essayant de vous projeter. N'hésitez pas à prendre votre temps!
-Ensuite vous vous verrez attribué une action et nous vous demanderons d'indiquer si vous penser que vous mettriez vraiment cette action en place ou si vous pensez que vous n'arriverez pas à vous y tenir. De la même façon, soyez le plus honnête et n'hésitez pas à prendre votre temps.
+Vous allez devoir juger les différentes propositions, nous vous demandons de le faire le plus honnêtement possible, en essayant de vous projeter. N'hésitez pas à prendre votre temps!
+Ensuite vous vous verrez attribuer une action et nous vous demanderons d'indiquer si vous pensez que vous mettrez vraiment cette action en place ou si vous pensez que vous n'arriverez pas à vous y tenir. De la même façon, soyez le plus honnête et n'hésitez pas à prendre votre temps.
 """
 ]
 
